9fed60bf0ad23c9a7a76bebca4093babc6f64c59
yunbo-z <zyunbo0330@gmail.com>
2024-02-21 19:17:37 +0100

    Add sort data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -18,19 +18,32 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 
-
+//GET /tasks?completed=true
+//GET /tasks?limit=10&skip=20
+//GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
-
+    const sort = {}
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] ==='desc' ? -1 : 1
     }
 
     try {
         // const tasks = await Tasks.find({ owner: req.user._id })
         await req.user.populate({
             path: 'task',
-            match
+            match,
+            options: {
+                //the value parsed in query is a string
+                limit: parseInt(req.query.limit),//a function provided by js, parse a string that contains a number into an actual integer which is what mongoose expect
+                skip: parseInt(req.query.skip),//skip the first few output
+                sort
+            }
         }) //??? why here after removing exec() everything works
         res.send(req.user.task)
     } catch (e) {
@@ -61,7 +74,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
         return res.status(400).send({ error: 'Invalid Update!' })
     }
     try {
-        
+
         const task = await Tasks.findOne({ _id: req.params.id, owner: req.user._id })
         // const task = await Tasks.findById(req.params.id)
         // const task = await Tasks.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
